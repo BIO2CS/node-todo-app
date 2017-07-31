984b4c37328df7458ed5fe20137ae55c01c26a8a
Huiqiong Gu <hgu@walmartlabs.com>
2017-07-30 23:54:47 -0700

    updating MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,10 @@ var mongoose = require("mongoose");
 
 const localUrl = "mongodb://localhost:27017/TodoApp";
 
-const mlabUrl = "mongodb://hgu29:anhui14004@ds127783.mlab.com:27783/hgu-node-todo-app";
+// const mlabUrl = "mongodb://hgu29:anhui14004@ds127783.mlab.com:27783/hgu-node-todo-app";
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mlabUrl || localUrl);
+mongoose.connect(process.env.MONGODB_URI || localUrl);
 
 module.exports = {
   mongoose
